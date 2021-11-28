059a3eed7c408abbe4b93064e31e41b11052b97f
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-11-28 10:07:13 +0900

    コントローラの処理

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class BookController extends Controller
 {
     public function index()
     {
-
+      $books = Book::all();
+      return view("books.index", compact("books"));
     }
 }
