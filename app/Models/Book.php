aaceb99c46beefbd300b8ace1db039c5a1c91f66
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-12-07 05:21:10 +0900

    Modelにfillableの追加

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,4 +11,8 @@ class Book extends Model
     static $categories = [
         "趣味", "食費", "光熱費", "家賃","ローン","交際費","教育費","給料","副業","臨時収入"
     ];
+
+    protected $fillable = [
+        "inout", "year", "month", "category", "amount", "memo"
+    ];
 }
