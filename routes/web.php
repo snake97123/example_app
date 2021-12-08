d6ef0058a8febc7c42c476d71c70b127eec897de
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-12-09 06:24:44 +0900

    routingの設定

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::resource("books", BookController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
